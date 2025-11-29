747f7dfdd84031007b9b8be9312af5427392155b
manish354200 <my562863@gmail.com>
2025-11-29 05:28:24 +0000

    project-1

---
+++ 1 Number Guessing Game/guessing_game.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() {
+    int random, guess;
+    int no_of_guess =0;
+    srand(time(NULL));
+    printf("welcome to the world of guessing number\n");
+    random = rand() % 100 +1; //Generating between 0 to 100
+    do{
+        printf("\nPlease enter your Guess between(1 to 100):");
+        scanf("%d",&guess);
+        no_of_guess++;
+        if(guess<random){
+            printf("Guess a larger number. \n");
+        }
+        else if(guess>random){
+             printf("guess a smaller number. \n");
+        }
+        else{
+            printf("Congratulations !!You have successfully guessed the Number is %d attempts", no_of_guess);
+        }
+
+    }
+    while (guess != random);
+    printf("\n Bye Bye, Thanks for Playing.");
+    printf("\n Developed by: Manishdeveoper");
+}
